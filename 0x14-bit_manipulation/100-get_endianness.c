026e0bde17436bd8eec51473f70ffe02a6e60993
Oddy-G <odettegovender@gmail.com>
2023-10-30 05:45:31 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * get_endianness - prog checks the endianness
+ * get_endianness - checks the endianness
  *
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-        int a;
-        char *b;
-        a = 1;
-        b = (char *)&a;
-        return (*b);
-} 
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
